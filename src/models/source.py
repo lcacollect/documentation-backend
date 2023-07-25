35dd2c8baa02647cc916c4fa453b0c0e11d5db93
Thomas Ammitzbøll-Bach <tab@uu.dk>
2023-07-25 14:37:23 +0200

    Fix reading CSV from Danish source

--- src/models/source.py
+++ src/models/source.py
@@ -49,8 +49,16 @@ class ProjectSource(SQLModel, table=True):
 
                     # reader: [{"col1_name": "row_val", "col2_name":..}, {...}]
                     if self.type == ProjectSourceType.CSV.value:
-                        file_data = StringIO(raw_data.decode())
-                        reader = pandas.read_csv(file_data).replace({np.nan: None}).to_dict("records")
+                        data = raw_data.decode()
+                        header = data[:data.index('\n')]
+                        if header.count(';') >= 2:
+                            sep = ';'
+                            dec = ','
+                        else:
+                            sep = ','
+                            dec = '.'
+                        file_data = StringIO(data)
+                        reader = pandas.read_csv(file_data, sep=sep, decimal=dec).replace({np.nan: None}).to_dict("records")
 
                     elif self.type == ProjectSourceType.XLSX.value:
                         reader = pandas.read_excel(raw_data).replace({np.nan: None}).to_dict("records")
